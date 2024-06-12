7ff4f6c1321ba8902e48053965402ae0053191c7
Vili Horvat <154683151+vhorvat22@users.noreply.github.com>
2024-06-12 21:48:37 +0200

    Add files via upload

---
+++ FinanceTracker/FinanceTracker/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FinanceTracker
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.BindingSource bindingSource;
+        private System.Windows.Forms.DataGridView dgvTransactions;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnAddTransaction;
+        private System.Windows.Forms.Label lblBalance;
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dgvTransactions = new System.Windows.Forms.DataGridView();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.btnAddTransaction = new System.Windows.Forms.Button();
+            this.lblBalance = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransactions)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTransactions
+            // 
+            this.dgvTransactions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTransactions.Location = new System.Drawing.Point(12, 70);
+            this.dgvTransactions.Name = "dgvTransactions";
+            this.dgvTransactions.Size = new System.Drawing.Size(360, 150);
+            this.dgvTransactions.TabIndex = 0;
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(12, 12);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtAmount.TabIndex = 1;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(118, 12);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(254, 20);
+            this.txtDescription.TabIndex = 2;
+            // 
+            // btnAddTransaction
+            // 
+            this.btnAddTransaction.Location = new System.Drawing.Point(12, 38);
+            this.btnAddTransaction.Name = "btnAddTransaction";
+            this.btnAddTransaction.Size = new System.Drawing.Size(75, 23);
+            this.btnAddTransaction.TabIndex = 3;
+            this.btnAddTransaction.Text = "Add";
+            this.btnAddTransaction.UseVisualStyleBackColor = true;
+            this.btnAddTransaction.Click += new System.EventHandler(this.btnAddTransaction_Click);
+            // 
+            // lblBalance
+            // 
+            this.lblBalance.AutoSize = true;
+            this.lblBalance.Location = new System.Drawing.Point(12, 230);
+            this.lblBalance.Name = "lblBalance";
+            this.lblBalance.Size = new System.Drawing.Size(52, 13);
+            this.lblBalance.TabIndex = 4;
+            this.lblBalance.Text = "Balance: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.lblBalance);
+            this.Controls.Add(this.btnAddTransaction);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.dgvTransactions);
+            this.Name = "Form1";
+            this.Text = "Finance Tracker";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransactions)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ FinanceTracker/FinanceTracker/Transaction.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Transaction class represents a single financial transaction.
+public class Transaction
+{
+    public decimal Amount { get; set; }
+    public string Description { get; set; }
+
+    public Transaction(decimal amount, string description)
+    {
+        Amount = amount;
+        Description = description;
+    }
+}
+
+// FinanceTrack class manages a list of transactions and provides functionality to add transactions and calculate balance.
+public class FinanceTrack
+{
+    private List<Transaction> transactions = new List<Transaction>();
+
+    public void AddTransaction(decimal amount, string description)
+    {
+        transactions.Add(new Transaction(amount, description));
+    }
+
+    public List<Transaction> ViewTransactions()
+    {
+        return transactions;
+    }
+
+    public decimal CalculateBalance()
+    {
+        return transactions.Sum(t => t.Amount);
+    }
+}
+
+// Program class contains the main method to demonstrate the functionality of FinanceTrack class.
+class Program
+{
+    static void Mein(string[] args)
+    {
+        FinanceTrack tracker = new FinanceTrack();
+
+        // Example usage
+        tracker.AddTransaction(1000, "Salary");
+        tracker.AddTransaction(-100, "Groceries");
+        tracker.AddTransaction(-50, "Electricity bill");
+
+        Console.WriteLine("Transactions:");
+        foreach (var transaction in tracker.ViewTransactions())
+        {
+            Console.WriteLine($"{transaction.Description}: {transaction.Amount:C}");
+        }
+
+        Console.WriteLine("Current Balance: " + tracker.CalculateBalance().ToString("C"));
+    }
+}
