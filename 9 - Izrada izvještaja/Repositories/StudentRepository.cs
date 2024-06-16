909bc9f556218aaf06279c302f60c363e3cfde69
Vili Horvat <154683151+vhorvat22@users.noreply.github.com>
2024-06-16 21:37:55 +0200

    Add files via upload

---
+++ 9 - Izrada izvještaja/Repositories/StudentRepository.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBLayer;
+using Evaluation_Manager.Models;
+
+namespace Evaluation_Manager.Repositories
+{
+    public class StudentRepository
+    {
+        public static Student GetStudent(int id)
+        {
+            Student student = null;
+            string sql = $"SELECT * FROM Students WHERE Id = {id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                student = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return student;
+        }
+
+        public static List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student> ();
+            string sql = "SELECT * FROM Students";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Student student = CreateObject(reader);
+                students.Add(student);
+            }
+            reader.Close();
+            DB.CloseConnection();
+            return students;
+        }
+
+        public static List<Student> GetStudentsWithoutGrade()
+        {
+            List<Student> students = new List<Student>();
+            string sql = "SELECT * FROM Students WHERE Grade = 0";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Student student = CreateObject(reader);
+                students.Add(student);
+            }
+            reader.Close();
+            DB.CloseConnection();
+            return students;
+        }
+
+        public static List<Student> GetStudentsWithParameters(string name, string surname, int grade)
+        {
+            List<Student> students = new List<Student>();
+            bool isCriteriaAppended = false;
+
+            string sql = "SELECT * FROM Students WHERE";
+
+            if (name != null)
+            {
+                sql += $" FirstName = '{name}'";
+                isCriteriaAppended = true;
+            }
+            if (surname != null)
+            {
+                if (isCriteriaAppended)
+                {
+                    sql += " AND";
+                }
+                sql += $" LastName = '{surname}'";
+                isCriteriaAppended = true;
+            }
+            if (grade != -1)
+            {
+                if (isCriteriaAppended)
+                {
+                    sql += " AND";
+                }
+                sql += $" Grade = {grade}";
+            }
+
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Student student = CreateObject(reader);
+                students.Add(student);
+            }
+            reader.Close();
+            DB.CloseConnection();
+            return students;
+        }
+
+        public static void InsertStudent(string firstName, string lastName)
+        {
+            string sql = $"INSERT INTO Students (FirstName, LastName, Grade) VALUES('{firstName}', '{lastName}', 0)";
+            DB.OpenConnection();
+            DB.ExecuteCommand(sql);
+            DB.CloseConnection();
+        }
+
+        private static Student CreateObject (SqlDataReader reader)
+        {
+            int id = int.Parse(reader["Id"].ToString());
+            string firstName = reader["FirstName"].ToString();
+            string lastName = reader["LastName"].ToString();
+            int grade = int.Parse(reader["Grade"].ToString());
+
+            var student = new Student
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+                Grade = grade,
+            };
+            return student;
+        }
+    }
+}
---
+++ 9 - Izrada izvještaja/Repositories/TeacherRepository.cs
@@ -0,0 +1,67 @@
+﻿using DBLayer;
+using Evaluation_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Repositories
+{
+    public class TeacherRepository
+    {
+        public static Teacher GetTeacher(string username)
+        {
+            string sql = $"SELECT * FROM Teachers WHERE Username = '{username}'";
+            return FetchTeacher(sql);
+        }
+
+        public static Teacher GetTeacher(int id)
+        {
+            string sql = $"SELECT * FROM Teachers WHERE Id = {id}";
+            return FetchTeacher(sql);
+        }
+
+        private static Teacher FetchTeacher(string sql)
+        {
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            Teacher teacher = null;
+            if (reader.HasRows == true)
+            {
+                reader.Read();
+                teacher = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return teacher;
+        }
+
+        public static void InsertTeacher(string firstName, string lastName, string username, string password)
+        {
+            string sql = $"INSERT INTO Teachers (FirstName, LastName, Username, Password) VALUES('{firstName}', '{lastName}', '{username}', '{password}')";
+            DB.OpenConnection();
+            DB.ExecuteCommand(sql);
+            DB.CloseConnection();
+        }
+
+        private static Teacher CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["Id"].ToString());
+            string firstName = reader["FirstName"].ToString();
+            string lastName = reader["LastName"].ToString();
+            string username = reader["Username"].ToString();
+            string password = reader["Password"].ToString();
+            var teacher = new Teacher
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+                Username = username,
+                Password = password
+            };
+            return teacher;
+        }
+    }
+}
